3dd8a0db5174efd48d64c93b22500f22f145c6c1
Liam Esteban Prince <liamesteban@gmail.com>
2022-05-17 14:36:22 +0200

    Refactor: Import ReactDOM from react-dom/client
    Instead of react-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function Square(props) {
